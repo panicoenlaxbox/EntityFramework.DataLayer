2cd777c50b46426b5c6ae4699b7d5c87660c7875
panicoenlaxbox <panicoenlaxbox@gmail.com>
2015-08-16 03:52:54 +0200

    Disconnected graphs http://www.entityframeworktutorial.net/EntityFramework5/update-entity-graph-using-dbcontext.aspx

--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -1,8 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Data.Entity;
-using System.Data.Entity.Infrastructure;
+using System.Linq;
 
 namespace ConsoleApplication1
 {
@@ -12,10 +13,68 @@ namespace ConsoleApplication1
         {
             Initialize();
 
-            using (var context = new ManagementContext())
-            {
-                var customer = context.Customers.Find(1);
-            }
+            //Country country;
+            //using (var context = new ManagementContext())
+            //{
+            //    country = context.Countries.First();
+            //}
+            ////country.State = State.Unchanged;
+            //Customer customer = new Customer()
+            //{
+            //    Name = "Customer 2",
+            //    State = State.Added,
+            //    Addresses = new Collection<Address>()
+            //    {
+            //        new Address()
+            //        {
+            //            Region = "Carabanchel",
+            //            CountryId = country.CountryId,
+            //            Country = country,
+            //            State = State.Added
+            //        }
+            //    }
+            //};
+            //using (var context = new ManagementContext())
+            //{
+            //    context.Customers.Add(customer);
+            //    context.ApplyStateChanges();
+            //    context.SaveChanges();
+            //}
+
+            //Customer customer;
+            //using (var context = new ManagementContext())
+            //{
+            //    customer = context.Customers
+            //        .Include(p => p.Addresses)
+            //        .Include(p => p.Addresses.Select(t => t.Country)).First();
+            //}
+            //customer.Name = "Customer 2";
+            //customer.State = State.Modified;
+            //var country = customer.Addresses.First().Country;
+            //customer.Addresses.Add(new Address()
+            //{
+            //    Region = "Carabanchel",
+            //    Country = country,
+            //    CountryId = country.CountryId,
+            //    State = State.Added
+            //});
+            //using (var context = new ManagementContext())
+            //{
+            //    context.Customers.Add(customer);
+            //    context.ApplyStateChanges();
+            //    context.SaveChanges();
+            //}
+
+            //Customer customer;
+            //using (var context = new ManagementContext())
+            //{
+            //    customer = context.Customers
+            //        .Include(p => p.Addresses)
+            //        .Include(p => p.Addresses.Select(t => t.Country)).First();
+            //    customer.Addresses.First().Country = context.Countries.Find(2);
+            //    context.ChangeTracker.DetectChanges();
+            //    context.SaveChanges();
+            //}
 
             Console.ReadKey();
         }
@@ -30,6 +89,101 @@ namespace ConsoleApplication1
         }
     }
 
+    public abstract class EntityWithState
+    {
+        [NotMapped]
+        public State State { get; set; }
+    }
+
+    public interface ICustomerRepository
+    {
+        void InsertOrUpdate(Customer customer);
+        void Remove(Customer customer);
+        IQueryable<Customer> GetAll();
+        Customer Find(int customerId);
+        void SaveChanges();
+    }
+
+    class CustomerRepository : ICustomerRepository, IDisposable
+    {
+        private readonly ManagementContext _context;
+
+        public CustomerRepository()
+        {
+            _context = new ManagementContext();
+        }
+
+        public void InsertOrUpdateGraph(Customer customer)
+        {
+
+        }
+
+        public void InsertOrUpdate(Customer customer)
+        {
+            if (customer.CustomerId == default(int))
+            {
+                _context.Customers.Add(customer);
+            }
+            else
+            {
+                _context.Entry(customer).State = EntityState.Modified;
+            }
+        }
+
+        public void Remove(Customer customer)
+        {
+            _context.Customers.Remove(customer);
+        }
+
+        public IQueryable<Customer> GetAll()
+        {
+            return _context.Customers;
+        }
+
+        public Customer Find(int customerId)
+        {
+            return _context.Customers.Find(customerId);
+        }
+
+        public void SaveChanges()
+        {
+            _context.SaveChanges();
+        }
+
+        #region IDisposable
+        bool _disposed;
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        ~CustomerRepository()
+        {
+            Dispose(false);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (_disposed)
+                return;
+
+            if (disposing)
+            {
+                // free other managed objects that implement
+                // IDisposable only
+                _context.Dispose();
+            }
+
+            // release any unmanaged objects
+            // set the object references to null
+
+            _disposed = true;
+        }
+        #endregion
+    }
+
     public class ManagementInitializer : DropCreateDatabaseAlways<ManagementContext>
     {
         protected override void Seed(ManagementContext context)
@@ -37,9 +191,12 @@ namespace ConsoleApplication1
             var country = new Country() { Name = "España" };
             context.Countries.Add(country);
 
+            context.Countries.Add(new Country() { Name = "USA" });
+
             var customer = new Customer()
             {
                 Name = "Customer 1",
+                Code = "C1",
                 Addresses = new Collection<Address>()
                 {
                     new Address() { Region = "Madrid", Country = country },
@@ -50,6 +207,17 @@ namespace ConsoleApplication1
         }
     }
 
+    public static class DbContextExtensions
+    {
+        public static void ApplyStateChanges(this DbContext context)
+        {
+            foreach (var entry in context.ChangeTracker.Entries<EntityWithState>())
+            {
+                entry.State = entry.Entity.State.ConvertToEntityState();
+            }
+        }
+    }
+
     public class ManagementContext : DbContext
     {
         public DbSet<Customer> Customers { get; set; }
@@ -57,7 +225,7 @@ namespace ConsoleApplication1
         public DbSet<Country> Countries { get; set; }
     }
 
-    public class Address
+    public class Address : EntityWithState
     {
         public int AddressId { get; set; }
         public string Region { get; set; }
@@ -67,17 +235,44 @@ namespace ConsoleApplication1
         public virtual Customer Customer { get; set; }
     }
 
-    public class Customer
+    public class Customer : EntityWithState
     {
         public int CustomerId { get; set; }
         public string Name { get; set; }
-        public ICollection<Address> Addresses { get; set; }
+        public string Code { get; set; }
+        public virtual ICollection<Address> Addresses { get; set; }
     }
 
-    public class Country
+    public class Country : EntityWithState
     {
         public int CountryId { get; set; }
         public string Name { get; set; }
-        public ICollection<Address> Addresses { get; set; }
+        public virtual ICollection<Address> Addresses { get; set; }
+    }
+
+    public enum State
+    {
+        Unchanged,
+        Added,
+        Deleted,
+        Modified
+    }
+
+    public static class StateExtensions
+    {
+        public static EntityState ConvertToEntityState(this State state)
+        {
+            switch (state)
+            {
+                case State.Added:
+                    return EntityState.Added;
+                case State.Deleted:
+                    return EntityState.Deleted;
+                case State.Modified:
+                    return EntityState.Modified;
+                default:
+                    return EntityState.Detached;
+            }
+        }
     }
 }
